769f3cbebfe6e4b3cca6aed190639f128b1a9edc
Kez Nwichi <143608228+CKZN1@users.noreply.github.com>
2024-03-12 18:16:43 +0000

    error handling

--- FSPBook.Portal/Services/NewsService.cs
+++ FSPBook.Portal/Services/NewsService.cs
@@ -20,14 +20,17 @@ namespace FSPBook.Portal.Services
         }
 
         public async Task<List<Datum>> GetNews()
-        {
+        { 
             var url = Configuration.GetSection("NewsAPIUrl").Value;
+            if(string.IsNullOrEmpty(url)) 
+                return new List<Datum>();
 
             var httpclient = _httpClientFactory.CreateClient();
             var response = await httpclient.GetAsync(url);
 
             var newsResponse = await response.Content.ReadAsStringAsync();
             var json = JsonSerializer.Deserialize<NewsAPIResponse>(newsResponse);
+
             if (json.data == null)
                 return new List<Datum>();
             return json.data.ToList();
